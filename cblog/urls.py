be41aa68ac0c3b549e5692ce898683903eeff5fb
cooper <davidontech@gmail.com>
2015-07-02 21:42:40 +1200

    edit and delete finished

--- cblog/urls.py
+++ cblog/urls.py
@@ -5,4 +5,6 @@ urlpatterns=patterns('',
     url(r'login/$', 'cblog.views.cblog_login',name='reverse_cblog_login'),
     url(r'logout/$', 'cblog.views.cblog_logout',name='reverse_cblog_logout'),
     url(r'edit/$', 'cblog.views.cblog_edit',name='reverse_cblog_edit'),
+    url(r'edit/(?P<id>\d*?)/$', 'cblog.views.cblog_edit',name='reverse_cblog_edit'),
+    url(r'delete/(?P<id>\d*?)$', 'cblog.views.cblog_delete',name='reverse_cblog_delete' ),
 )
--- cblog/views.py
+++ cblog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render_to_response, HttpResponseRedirect, redirect, render
+from django.shortcuts import render_to_response, HttpResponseRedirect, redirect, render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.models import User
 from django.template import RequestContext
+from django.http import Http404
 from cblog.models import Entry, Category
 from cblog.config import setting
 from cblog.forms import EntryForm
@@ -23,26 +24,47 @@ def cblog_index(request):
                        {"entry_list": Entry.objects.all(),
                         "category_list": Category.objects.all(),
                         "setting": setting,
-                        "user": User}
+                        "user":User}
                        )
     return render_to_response("cblog/cblog_index.html", c)
 
 
 @login_required(login_url="/cblog/login")
-def cblog_edit(request):
+def cblog_edit(request, id=""):
+
+    if id:
+        entry=get_object_or_404(Entry, pk=id)
+    else:
+        entry=None
+
     if request.method=='POST':
-        form=EntryForm(request.POST)
+        form=EntryForm(request.POST,instance=entry)
         if form.is_valid():
             form.save()
-        else:
-            print(form)
     else:
-        form=EntryForm()
+        form=EntryForm(instance=entry)
 
-#    c={"user": User,"setting":setting,"form":form}
     c = RequestContext(request,
                        {"form":form,
                         "setting": setting,
+                        "id": id,
                         "user": User}
                        )
     return render_to_response("cblog/cblog_edit.html",c)
+
+@login_required(login_url="/cblog/login")
+def cblog_delete(request, id):
+
+    try:
+        entry=Entry.objects.get(pk=id)
+        if entry.isdraft:
+            entry.delete()
+        else:
+            entry.isdraft=True
+            entry.save(update_fields=['isdraft'])
+
+    except Entry.DoesNotExist:
+        raise Http404()
+
+    return redirect('%s' %reverse(cblog_index))
+
